d6053c28f4c48a5fbee06c46bb2100831918a87b
SteakingCoder <1941499609@qq.com>
2018-05-06 18:40:04 +0800

    完成基本的请求响应

--- src/main/java/com/controller/BasicController.java
+++ src/main/java/com/controller/BasicController.java
@@ -1,7 +1,13 @@
 package com.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.sql.*;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by HuangDanGeeker on 2018/4/25.
@@ -9,13 +15,57 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class BasicController {
 
-    @RequestMapping("/test/index")
-    public String test(){
-        return "index";
+    private static final String driver = "com.mysql.jdbc.Driver";
+    private static final String url = "jdbc:mysql://localhost:3306/blog?useUnicode=true&characterEncoding=UTF-8&useSSL=false&serverTimezone=Hongkong";
+    private static final String username = "root";
+    private static final String password = "";
+    private static Connection conn = null;
+
+    static
+    {
+        try
+        {
+            Class.forName(driver);
+            conn = DriverManager.getConnection(url, username, password);
+            System.out.println("success");
+        }
+        catch(Exception ex)
+        {
+            ex.printStackTrace();
+            System.out.println("wrong");
+        }
     }
 
-    @RequestMapping("/test/login")
-    public String testLogin(){
+    @RequestMapping("/blog/login")
+    public String loginPage(){
         return "/admin/login";
+    }
+
+
+    @RequestMapping("/login/{userId}/{userPw}")
+    @ResponseBody
+    public Map testLogin(@PathVariable("userId") String userId, @PathVariable("userPw") String userPw){
+
+        Map result = new HashMap<String, Object>(5);
+        try {
+            Statement statement = conn.createStatement();
+            ResultSet resultSet = statement.executeQuery("select password from user where id='"+userId+"';'");
+            if( resultSet.next() ){
+                if(resultSet.getString(0).equals(userPw)){
+                    result.put("loginStatus", "success");
+                    return result;
+                }
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        result.put("loginStatus", "failure");
+        return result;
+    }
+
+    @RequestMapping("/blog/profile")
+    public String testProfile(){
+        return "/admin/profile";
     }
 }
--- src/main/resources/db_init.sql
+++ src/main/resources/db_init.sql
@@ -1,28 +1,16 @@
-create
-use blog_system;
+use blog;
 
 -- 建表
 -- 1.创建员工表
 drop table staff;
 create TABLE staff (name VARCHAR(20), no VARCHAR(20) PRIMARY KEY, passwd INT DEFAULT 111111, birthday VARCHAR(20), phone VARCHAR(20)) CHARSET=utf8;
--- 2.创建客户表
-drop table customer;
-create TABLE customer (name VARCHAR(20), no VARCHAR(20) PRIMARY KEY, passwd INT DEFAULT 111111,birthday VARCHAR(20), phone VARCHAR(20), address VARCHAR(40)) CHARSET=utf8;
--- 3.创建客户操作记录表
-create TABLE recd_custm_0902150228(no VARCHAR(20) PRIMARY KEY, operType INT, nums INT, dealerNo VARCHAR(20) DEFAULT '0', dealerName VARCHAR(20) DEFAULT '0', operDate DATETIME);
--- 4.创建柜员操作记录表
-create TABLE recd_staff_1(operType VARCHAR(20), custmNo VARCHAR(20) NOT NULL, custmName VARCHAR(20) NOT NULL, creditCardNo varchar(20) NOT NULL ,nums INT, operDate VARCHAR(20));
-drop TABLE  recd_staff_1;
--- 5.创建用户账号下所有的银行卡存款表
-create TABLE  creditcard_0902150228(cardNo VARCHAR(20) PRIMARY KEY NOT NULL , nums INT DEFAULT 0, intrest FLOAT DEFAULT 0, dueTime VARCHAR(20));
--- 6.创建信用卡号&&账号集(存储最新创建的账号和信用卡账号)
-create TABLE  num_set(no VARCHAR(20) PRIMARY KEY, type INT DEFAULT 0);
--- 7.创建存储利率表
-create TABLE  rate_table(type VARCHAR(20) PRIMARY KEY, rate FLOAT DEFAULT 0);
+
+
+
+
 -- 数据操作
--- 1.插入员工
-INSERT INTO staff VALUES ('HuangDan', 0902150228, 111111, 970101, 18373151462);
-INSERT INTO staff VALUES ('1', 1, 1, 970101, 18373151462);
+-- 1.插入user
+INSERT INTO user VALUES ("123", "123", "123", "123", "123", "123");
 
 -- 2.插入客户
 INSERT INTO customer VALUES ('HuangDan', 0902150228, 111111, 970101, 18373151462, '中南大学-升华公寓');
